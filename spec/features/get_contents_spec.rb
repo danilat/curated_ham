d34c88f57342525609dfd6d00814bb266a589aa2
Dani Latorre <dani@danilat.com>
2016-09-30 19:46:51 +0200

    changed acceptation test, now we execute an integration test with the action as entry point

--- spec/features/get_contents_spec.rb
+++ spec/features/get_contents_spec.rb
@@ -1,64 +1,18 @@
-require 'json'
+describe "Get links", :feature do
+  let(:the_coding_stones) do
+    {title: "coding stones", url: "http://codingstones.com/", category: "rockanroll"}
+  end
 
-describe "Get contents", :feature do
   before(:each) do
-    @my_json_api = API.new
+    link_repository = CuratedHam::LinkRepository.new
+    @retrieve_action = CuratedHam::RetrieveAllTheLinks.new(link_repository)
   end
 
-  context "in JSON format" do
-    let(:the_coding_stones) do
-      {title: "coding stones", url: "http://codingstones.com/", category: "rockanroll"}
-    end
+  it "returns a collection" do
+    links = @retrieve_action.run
+    link = links.first
 
-    before(:each) do
-      @my_json_api.post(the_coding_stones)
-    end
-
-    it "gets a collection" do
-      result = @my_json_api.get
-
-      expect(result).not_to be_nil
-      expect(JSON.parse(result).size).to eq 1
-    end
-
-    it "items has the right format" do
-      result = @my_json_api.get
-      json_result = JSON.parse(result, symbolize_names: true)
-      item = json_result.first
-
-      expect(item[:title]).to eq the_coding_stones[:title]
-      expect(item[:url]).to eq the_coding_stones[:url]
-      expect(item[:category]).to eq the_coding_stones[:category]
-      expect(item[:created_at]).not_to be_nil
-    end
+    expect(link).to have_attributes(the_coding_stones)
   end
 
-end
-
-class API
-  def initialize
-    
-  end
-
-  def post(content_params)
-    
-  end
-
-  def get
-    
-  end
-
-  private 
-    def content_json_serializer(contents)
-      contents_hash = contents.collect do |content| 
-        {
-          title: content.title,
-          url: content.url,
-          description: content.description,
-          category: content.category,
-          created_at: content.created_at
-        }
-      end
-      JSON.dump(contents_hash)
-    end
 end
