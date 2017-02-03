6c6d756fb12565b34be19a4f9763546a7cfdb049
Dani Latorre <dani@danilat.com>
2017-02-03 12:42:02 +0100

    fixed feature spec

--- spec/features/get_contents_spec.rb
+++ spec/features/get_contents_spec.rb
@@ -5,8 +5,10 @@ describe "Get links", :feature do
 
   before(:each) do
     link_repository = CuratedHam::LinkRepository.new
+    clock = CuratedHam::Clock.new
+    link_service = CuratedHam::LinkService.new(link_repository, clock)
     @retrieve_action = CuratedHam::RetrieveAllTheLinks.new(link_repository)
-    @create_action = CuratedHam::CreateALink.new
+    @create_action = CuratedHam::CreateALink.new(link_service)
   end
 
   it "returns a collection" do
